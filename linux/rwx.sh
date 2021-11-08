f772c17d8486260718a25cde46d7618e861b3a96
ernanecl <ernanecl@gmail.com>
2021-11-07 21:13:26 -0300

    working with reading, writing and executing files

---
+++ linux/rwx.sh
@@ -0,0 +1,12 @@
+# comandos de leitura, escrita e execução de arquivos
+
+# ver permissões de arquivos e diretórios
+    ll
+    ls -l
+    ls -la
+
+# dar permissão
+    chmod +x file_name.sh
+
+# executar o arquivo shell
+    ./file_name.sh
