5e3e8e3e1f25b2f94a715b823b0e0ac4174e0f4c
ernane <26651736+ernanecl@users.noreply.github.com>
2024-04-09 16:48:53 -0300

    Update ifconfig.sh

--- linux/system_resources/ifconfig.sh
+++ linux/system_resources/ifconfig.sh
@@ -1,26 +1,5 @@
-# "https://www.how2shout.com/linux/install-ifconfigon-debian-11-or-10-if-command-not-found/"
-# the "ifconfig" command is deprecated, but follows its installation for use
-# Install "ifconfig" on "Debian 11" or "10", if command not found
+# link of tutorial for install "ifconfig" on "Debian 10" and "Debian 11"
+# https://www.how2shout.com/linux/install-ifconfigon-debian-11-or-10-if-command-not-found/
 
-# Although we don’t need to install "ipconfig",
-# because we already have an inbuilt tool to check the ip-address that is:
-$ ip a
-
-# Run system update and command to install ifconfig on "Debian"
-$ sudo apt update
-$ sudo apt install net-tools
-
-# Add "ifconfig" to the system path
-# By default, you won’t be able to use the command because it is not in the system path.
-# If you don’t want to add it to the path then use
-$ sbin/ifconfig
-
-# However, for ease, us the below steps:
-# The text editor can be "nano", "vim", "vi", among others
-$ sudo <text_editor> ~/.bashrc
-
-# Scroll to the end of the file and add
-$ export PATH=/usr/sbin:$PATH
-
-# Use the ifconfig
-$ ifconfig
+# check the terminal IP
+$ sudo ifconfig
