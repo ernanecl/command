77997d9adab01d31ead24bbc9f2d573642ad7463
ernane <26651736+ernanecl@users.noreply.github.com>
2024-04-09 01:41:57 -0300

    Update ifconfig.sh

--- linux/system_resources/ifconfig.sh
+++ linux/system_resources/ifconfig.sh
@@ -1,16 +1,16 @@
 # "https://www.how2shout.com/linux/install-ifconfigon-debian-11-or-10-if-command-not-found/"
 # the "ifconfig" command is deprecated, but follows its installation for use
-# Install ifconfig on Debian 11 or 10, if command not found
+# Install "ifconfig" on "Debian 11" or "10", if command not found
 
-# Although we don’t need to install ipconfig,
+# Although we don’t need to install "ipconfig",
 # because we already have an inbuilt tool to check the ip-address that is:
 $ ip a
 
-# Run system update and command to install ifconfig on Debian
+# Run system update and command to install ifconfig on "Debian"
 $ sudo apt update
 $ sudo apt install net-tools
 
-# Add ifconfig to the system path
+# Add "ifconfig" to the system path
 # By default, you won’t be able to use the command because it is not in the system path.
 # If you don’t want to add it to the path then use
 $ sbin/ifconfig
