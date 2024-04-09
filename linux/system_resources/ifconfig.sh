f53c37460c39a01759d5a4f906602351de277f45
ernane <26651736+ernanecl@users.noreply.github.com>
2024-04-09 16:49:18 -0300

    Update ifconfig.sh

--- linux/system_resources/ifconfig.sh
+++ linux/system_resources/ifconfig.sh
@@ -1,5 +1,6 @@
 # link of tutorial for install "ifconfig" on "Debian 10" and "Debian 11"
 # https://www.how2shout.com/linux/install-ifconfigon-debian-11-or-10-if-command-not-found/
 
+# https://www.djpdv.com.br/manuais/linux/91VerificarIPdoterminal.html#:~:text=Verificar%20IP%20do%20terminal,-9.&text=No%20console%20Linux%20execute%20o,Imagem%2032%20%2D%20IP%20do%20Terminal.
 # check the terminal IP
 $ sudo ifconfig
