ff10e471f94ca4f64109c757392ac0665f9e9a36
ernane <26651736+ernanecl@users.noreply.github.com>
2024-04-23 17:02:08 -0300

    Create bookmark.ps1

---
+++ windows/apps/chrome/bookmark.ps1
@@ -0,0 +1,20 @@
+# check current directory
+pwd
+
+# get the current username
+(Get-CimInstance -ClassName Win32_ComputerSystem).UserName.Split('\')[-1]
+
+# access your user directory
+cd C:\Users\<username>
+
+# list all files in the user directory, alse can to use ls or dir commands
+Get-ChildItem . -Force
+# or
+Get-ChildItem -Force
+
+# access bookmark directory
+cd '.\AppData\Local\Google\Chrome\User Data\Default'
+
+# files bookmarks
+Bookmarks
+Bookmarks.bak
